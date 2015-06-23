e42bcec04d431cfe9566c4801c82fc2386c33331
trodrigu <almkondMUSIC1983>
2015-06-23 12:36:26 -0700

    updated home and added registration validations

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,4 @@
 class Phone < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :phone
+  validates_presence_of :phone, format: { with: /^1*[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/}
 end
