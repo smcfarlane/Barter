51b688e098914d116886686f32f629e7d51e9790
trodrigu <trodriguez91@icloud.com>
2015-07-01 13:29:57 -0700

    Take out excess code in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,21 +1,10 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
-  respond_to :html, :js
 
   def index
     @skills = Skill.all
     @boards = Board.where(status: 'awaiting')
     @users = User.all
-  end
-
-  def delete_skill
-    @skills = Skill.all
-  end
-
-  def destroy_skill
-    SkillsUser.where(skill_id: params[:name]).delete_all
-    Skill.destroy(params[:name])
-    redirect_to skills_path
   end
 
   def destroy_user
