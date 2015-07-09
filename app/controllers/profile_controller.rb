b689f5e006b3ce0bcb2ff91b9843f638cdd6e87f
Warren <warrendevs@users.noreply.github.com>
2015-07-09 15:39:55 -0700

    Updates Profile index to display boards created by user.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -13,6 +13,11 @@ class ProfileController < ApplicationController
 
     @agreements = initiator + secondary
 
+    # Find boards created
+    @boards_created = Board.where("user_id=?", current_user.id)
+    # Sort by create date desc (newest board entries first)
+    @boards_created.sort{|a,b| b[:created_at] <=> a[:created_at]}
+
     # Find boards looking for user's skills
     @boards_match =  Board.where(@skills.map {|s| "'#{s.name}' = ANY (skill_needed)"}.join(' OR ') ).limit(10)
 
