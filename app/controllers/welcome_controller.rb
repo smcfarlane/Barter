dbd717ff47c0b43e5f2f38de2ee8323fdfbef974
therahl <JAYHENNESSY@GMAIL.COM>
2015-07-17 12:44:23 -0700

    added demo user data to welcome page through a modal

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   include ApplicationHelper
   def index
+    @user_list = User.all.limit(10).order('created_at ASC')
+    @admin = @user_list.where(admin:true)
+    
     redirect_to profile_path if current_user
   end
 end
